5c3a9978032261bdfc5577176a9413e22109b25f
pedro-friedrich <pedroafriedrich@gmail.com>
2025-04-09 11:16:11 -0300

    Waypoint System

---
+++ Assets/Scripts/ObjectiveWaypoint.cs
@@ -0,0 +1,58 @@
+using TMPro;
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ObjectiveWaypoint : MonoBehaviour
+{
+    public Image img;
+    public Transform target;
+    public TextMeshProUGUI meter;
+    public Vector3 offset;
+
+    private Player player;
+
+    void Start()
+    {
+        player = FindAnyObjectByType<Player>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float minX = img.GetPixelAdjustedRect().width / 2;
+        float maxX = Screen.width - minX;
+
+        float minY = img.GetPixelAdjustedRect().height / 2;
+        float maxY = Screen.height - minY;
+
+        Vector2 pos = Camera.main.WorldToScreenPoint(target.position + offset);
+
+        if (Vector3.Dot(target.position - transform.position, transform.forward) < 0)
+        {
+            if (pos.x < Screen.width / 2)
+            {
+                pos.x = maxX;
+            }
+            else
+            {
+                pos.x = minX;
+            }
+
+            if (pos.y < Screen.height / 2)
+            {
+                pos.y = maxY;
+            }
+            else
+            {
+                pos.y = minY;
+            }
+        }
+
+        pos.x = Mathf.Clamp(pos.x, minX, maxX);
+        pos.y = Mathf.Clamp(pos.y, minY, maxY);
+
+        img.transform.position = pos;
+        meter.text = Vector3.Distance(target.position, player.transform.position).ToString("0") + "m";
+    }
+}
